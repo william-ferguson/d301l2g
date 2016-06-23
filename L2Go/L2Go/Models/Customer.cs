ef7332b45fa3d8dc3140a3691665a4772989845f
william ferguson <willyferguson@hotmail.co.nz>
2016-06-23 16:08:38 +1200

    working

---
+++ L2Go/L2Go/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2Go
+{
+    class Customer
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string PhoneNumber { get; set; }
+        string City { get; set; }
+        string EmailAddress { get; set; }
+        string CreditCardNumber { get; set; }
+        string CreditCardExpiryMonth { get; set; }
+        string CreditCardExpiryYear { get; set; }
+        string CreditCardSecurityCode { get; set; }
+
+
+
+    }
+}
