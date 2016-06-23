487c62bd7cb40ae272f90a97d16ab0e4abcde7b8
william ferguson <willyferguson@hotmail.co.nz>
2016-06-23 17:58:41 +1200

    working still

--- L2Go/L2Go/Models/Customer.cs
+++ L2Go/L2Go/Models/Customer.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace L2Go
 {
     class Customer
     {
-        string Name { get; set; }
-        string Address { get; set; }
-        string PhoneNumber { get; set; }
-        string City { get; set; }
-        string EmailAddress { get; set; }
-        string CreditCardNumber { get; set; }
-        string CreditCardExpiryMonth { get; set; }
-        string CreditCardExpiryYear { get; set; }
-        string CreditCardSecurityCode { get; set; }
+          [PrimaryKey, AutoIncrement]
+          public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string CreditCardExpiryMonth { get; set; }
+        public string CreditCardExpiryYear { get; set; }
+        public string CreditCardSecurityCode { get; set; }
 
 
 
--- L2Go/L2Go/Regestration_Page.xaml.cs
+++ L2Go/L2Go/Regestration_Page.xaml.cs
@@ -32,9 +32,106 @@ namespace L2Go
             this.Frame.Navigate(typeof(MainPage));
         }
 
-        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        private async void btnFinish_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(MainPage));
+            string isValid = checkInput();
+            if (isValid == "valid")
+            {
+                Customer c = new Customer();
+                c.Name = tbxNameInput.Text;
+                c.Address = tbxAddressInput.Text;
+                c.PhoneNumber = tbxPhoneNumberInput.Text;
+                c.City = tbxCityInput.Text;
+                c.EmailAddress = tbxEmailAddressInput.Text;
+                c.CreditCardNumber = tbxCreditCardNumberInput.Text;
+                c.CreditCardExpiryMonth = tbxCCExpiryMonthInput.Text;
+                c.CreditCardExpiryYear = tbxCCExpiryYearInput.Text;
+                c.CreditCardSecurityCode = tbxCCSecurityCodeInput.Text;
+
+
+                this.Frame.Navigate(typeof(MainPage));
+            }
+            else
+            {
+                var message = new Windows.UI.Popups.MessageDialog(isValid, "Error");
+                message.Commands.Add(new Windows.UI.Popups.UICommand
+                {
+                    Label = "ok",
+                    Id = 0
+                });
+                await message.ShowAsync();
+            }
+
         }
+        private string checkInput()
+        {
+            string result = "valid";
+            if (string.IsNullOrWhiteSpace(tbxNameInput.Text))
+                return "Please insert correct name";
+
+            
+            if (string.IsNullOrWhiteSpace(tbxAddressInput.Text))
+                return "Please insert correct address";
+
+            
+            if (string.IsNullOrWhiteSpace(tbxPhoneNumberInput.Text))
+                return "Please insert correct phone number";
+
+            
+            if (string.IsNullOrWhiteSpace(tbxCityInput.Text))
+                return "Please insert correct city";
+
+            
+            if (string.IsNullOrWhiteSpace(tbxEmailAddressInput.Text))
+                return "Please insert correct email address";
+
+            if (string.IsNullOrWhiteSpace(tbxCreditCardNumberInput.Text))
+                return "Please insert correct credit card number";
+
+            if (string.IsNullOrWhiteSpace(tbxCCExpiryMonthInput.Text))
+                return "Please insert correct expiry month";
+
+            if (string.IsNullOrWhiteSpace(tbxCCExpiryYearInput.Text))
+                return "Please insert correct expiry year";
+
+            if (string.IsNullOrWhiteSpace(tbxCCSecurityCodeInput.Text))
+                return "Please insert a security code";
+
+            if (checkCreditCardInput(tbxCCSecurityCodeInput.Text).Length != 3) 
+                return "Please insert the correct security code";
+
+            if (checkCreditCardInput(tbxPhoneNumberInput.Text).Length < 7 || (checkCreditCardInput(tbxPhoneNumberInput.Text).Length > 15))
+                return "Please insert the correct phone number";
+
+            if (checkCreditCardInput(tbxCCExpiryMonthInput.Text).Length != 2)
+                return "Please insert the correct expiry month";
+
+            if (checkCreditCardInput(tbxCCExpiryYearInput.Text).Length != 2)
+                return "Please insert the correct expiry year";
+
+
+            return result;
+
+        
+        }
+
+        private string checkCreditCardInput(string Input)
+        {
+            Input = Input.Trim();
+            string checkCreditCardInput = "";
+
+            foreach (char c in Input)
+            {
+                if (char.IsNumber(c))
+                {
+                    checkCreditCardInput += c;
+                }
+            }
+
+            return checkCreditCardInput;
+               
+        }
+
+
     }
 }
