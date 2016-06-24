c9f66d8230935a41d48290821d385115c2d5ea5d
william ferguson <willyferguson@hotmail.co.nz>
2016-06-24 18:17:14 +1200

    works like a peach

--- L2Go/L2Go/Delivery time.xaml.cs
+++ L2Go/L2Go/Delivery time.xaml.cs
@@ -26,5 +26,10 @@ namespace L2Go
         {
             this.InitializeComponent();
         }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(page_2));
+        }
     }
 }
